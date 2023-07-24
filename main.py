2f00e6b110b1734224c52bd98468e3f72747542e
Yashu Singhai <111460054+yashusi@users.noreply.github.com>
2023-07-24 13:53:24 -0400

    Added an additional intro screen to ask either one player to two player mode, screen is functional but needs GUI improvements Also added an AI bot for the one player mode that will move around and shoot at players, AI Bot movement is functional but nees improvments to be more natural and able to shoot

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
 import os
+import sys
+import random
 pygame.font.init()
+pygame.init()
 
 #vd is for Vulture Driod and vn is for Venator Class Cruiser
 
@@ -16,7 +19,7 @@ VEL = 5                   #rate of movement for our objects/surfaces
 BULLET_VEL = 7            #rate of movement for bullets
 MAX_BULLETS = 5           #number of bullets that can be shot at once from each side
 SURFACE_WIDTH, SURFACE_HEIGHT = 120, 80     #dimensions of the surfaces (vd and vn)
-
+BUTTON_WIDTH, BUTTON_HEIGHT = 100, 30
 #creating events in cases that either is hit
 VD_HIT = pygame.USEREVENT + 1     
 VN_HIT = pygame.USEREVENT + 2
@@ -55,12 +58,12 @@ def venator_handle_movement(keys, vn):
 
 def draw_window(vd, vn, vd_bullets, vn_bullets, vd_health, vn_health):
     WIN.blit(BACKGROUND, (0,0))
-    pygame.draw.rect(WIN, (255, 255, 255), BORDER)
+    pygame.draw.rect(WIN, (255, 255, 255), BORDER)  #divider
 
     vd_health_text = HEALTH_FONT.render("Health:" + str(vd_health), 1, (255,255,255))
     vn_health_text = HEALTH_FONT.render("Health:" + str(vn_health), 1, (255,255,255))
-    WIN.blit(vd_health_text, ((WIDTH - vd_health_text.get_width() - 10), 10))
-    WIN.blit(vn_health_text, (10, 10))
+    WIN.blit(vd_health_text, (10, 10))
+    WIN.blit(vn_health_text, ((WIDTH - vd_health_text.get_width() - 10), 10))
 
     WIN.blit(VDROID, (vd.x, vd.y))
     WIN.blit(VENATOR, (vn.x, vn.y))
@@ -93,8 +96,59 @@ def draw_winner(text):
     pygame.display.update()
     pygame.time.delay(5000)
             
-#code the handles main loop and functions
+def draw_button(text, x, y):
+    print("draw")
+    button_rect = pygame.Rect(x, y, BUTTON_WIDTH, BUTTON_HEIGHT)
+    pygame.draw.rect(WIN, (0,140,140), button_rect)
+    text_surface = HEALTH_FONT.render(text, True, (255,255,255))
+    text_rect = text_surface.get_rect(center=button_rect.center)
+    WIN.blit(text_surface, text_rect)
+    print("draw 2")
+    return button_rect
+
+
 def main():
+    one_p = False
+    two_p = False
+    WIN.fill((255,255,255))
+    one_player_button = draw_button("1 Player", WIDTH // 4, HEIGHT // 2 - BUTTON_HEIGHT // 2)
+    two_players_button = draw_button("2 Players", WIDTH // 4 * 3, HEIGHT // 2 - BUTTON_HEIGHT // 2)
+    pygame.display.update()
+    clock = pygame.time.Clock()
+    while True:
+        clock.tick(FPS)
+        #if exit is pressed
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+        # Check button clicks
+        mouse_pos = pygame.mouse.get_pos()
+        mouse_clicked = pygame.mouse.get_pressed()[0]
+
+        if one_player_button.collidepoint(mouse_pos) and mouse_clicked:
+            print("1 Player selected")
+            one_p = True
+            break
+
+        elif two_players_button.collidepoint(mouse_pos) and mouse_clicked:
+            print("2 Players selected")
+            two_p = True
+            break
+    if one_p:
+        print("one player")
+        one_player_main()
+    elif two_p:
+        print("two player")
+        two_player_main()
+    else:
+        print("something went wrong at this place 1")
+        
+
+
+    
+#code the handles main loop and functions
+def two_player_main():
     vd = pygame.Rect(100, 300, SURFACE_WIDTH, SURFACE_HEIGHT)
     vn = pygame.Rect(700, 300, SURFACE_WIDTH, SURFACE_HEIGHT)
 
@@ -150,6 +204,69 @@ def main():
         draw_window(vd, vn, vd_bullets, vn_bullets, vd_health, vn_health)
         
     pygame.quit()
+
+
+def vdroid_bot_handle_movement(vd_bot, vn):
+    
+    vd_bot.x += random.randint(-2,2)
+    if vd_bot.centery < vn.centery:
+        vd_bot.y += VEL
+    elif vd_bot.centery > vn.centery:
+        vd_bot.y -= VEL
+
+
+def one_player_main():
+    vd_bot = pygame.Rect(100, 300, SURFACE_WIDTH, SURFACE_HEIGHT)
+    vn = pygame.Rect(700, 300, SURFACE_WIDTH, SURFACE_HEIGHT)
+
+    vd_bullets = []
+    vn_bullets = []
+
+    vd_health = 10
+    vn_health = 10
+
+    key_pressed = False
+    clock = pygame.time.Clock()
+    run = True
+    while(run): #while loop that makes sure game doesn't immedietly opens and closes
+        clock.tick(FPS)
+        for event in pygame.event.get():  #gets list of all events and iterates through each event
+            if event.type == pygame.QUIT:
+                run = False
+        
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RCTRL and len(vn_bullets) <= MAX_BULLETS and not key_pressed:
+                    key_pressed = True
+                    bullet = pygame.Rect(vn.x, vn.y + vn.height//2 - 2, 10, 5)
+                    vn_bullets.append(bullet)
+            if event.type == pygame.KEYUP: 
+                if event.key == pygame.K_RCTRL:
+                    key_pressed = False
+            if event.type == VD_HIT:
+                vd_health -= 1
+            if event.type == VN_HIT:
+                vn_health -= 1
+
+        
+        winner_text = ""
+        if vd_health <= 0:
+            winner_text = "Venator Wins!"
+
+        if vn_health <= 0:
+            winner_text = "Vulture Droid Wins!"
+        
+        if(winner_text != ""):
+            draw_winner(winner_text)
+            break
+
+        keys_pressed = pygame.key.get_pressed()
+        vdroid_bot_handle_movement(vd_bot, vn)
+        venator_handle_movement(keys_pressed, vn)
+        handle_bullets(vd_bullets, vn_bullets, vd_bot, vn)
+        draw_window(vd_bot, vn, vd_bullets, vn_bullets, vd_health, vn_health)
+        
+    pygame.quit()
+
 
 if __name__ == "__main__":
     main()
